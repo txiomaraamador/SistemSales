9d2aadff6d5e42298a5b63446f90cc32013734f2
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-02-09 13:46:57 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemSales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
