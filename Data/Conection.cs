e7ec3409e98cc5adcfcf7a6676a7b3bc67304fbe
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-02-09 14:18:43 -0600

    create models and conection to database

---
+++ Data/Conection.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SistemSales.Data
+{
+    public class Conection
+    {
+        private string chainMySql = string.Empty;
+
+        public Conection()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            chainMySql = builder.GetSection("ConnectionStrings:ConectionMySql").Value;
+        }
+
+        public string getChainMySql(){
+            return chainMySql;
+        }
+    }
+}
