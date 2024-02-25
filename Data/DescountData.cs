c4010751503a976f9046ff8241a6955027b445b6
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-02-24 21:08:33 -0600

    crud descounts

---
+++ Data/DescountData.cs
@@ -0,0 +1,149 @@
+﻿using MySql.Data.MySqlClient;
+using SistemSales.Models;
+using System.Data;
+
+namespace SistemSales.Data
+{
+    public class DescountData
+    {
+        public List<DescountModel> ShowDescounts()
+        {
+            var oList = new List<DescountModel>();
+
+            var cn = new Conection();
+
+            using (var conection = new MySqlConnection(cn.getChainMySql()))
+            {
+                conection.Open();
+                MySqlCommand cmd = new MySqlCommand("SpShowDescounts", conection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oList.Add(new DescountModel()
+                        {
+                            IdDescount = Convert.ToInt32(dr["id"]),
+                            Code = dr["code"].ToString(),
+                            Cant = dr["cant"].ToString(),
+                            
+                        });
+                    }
+                }
+            }
+            return oList;
+        }
+        //Obtener
+        public DescountModel GetDescount(int IdDescount)
+        {
+            var oDescount = new DescountModel();
+
+            var cn = new Conection();
+
+            using (var conection = new MySqlConnection(cn.getChainMySql()))
+            {
+                conection.Open();
+                MySqlCommand cmd = new MySqlCommand("SpGetDescount", conection);
+                cmd.Parameters.AddWithValue("p_id", IdDescount);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+
+                        oDescount.IdDescount = Convert.ToInt32(dr["id"]);
+                        oDescount.Code = dr["code"].ToString();
+                        oDescount.Cant = dr["cant"].ToString();
+                       
+
+                    }
+                }
+            }
+            return oDescount;
+        }
+        //CREATE
+        public bool CreateDescount(DescountModel oDescount)
+        {
+            bool rpta;
+            try
+            {
+                var cn = new Conection();
+
+                using (var conection = new MySqlConnection(cn.getChainMySql()))
+                {
+                    conection.Open();
+                    MySqlCommand cmd = new MySqlCommand("SpCreateDescount", conection);
+                    cmd.Parameters.AddWithValue("p_code", oDescount.Code);
+                    cmd.Parameters.AddWithValue("p_cant", oDescount.Cant);
+                 
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+        //EDIT
+        public bool EditDescount(DescountModel oDescount)
+        {
+            bool rpta;
+            try
+            {
+                var cn = new Conection();
+
+                using (var conection = new MySqlConnection(cn.getChainMySql()))
+                {
+                    conection.Open();
+                    MySqlCommand cmd = new MySqlCommand("SpEditDescount", conection);
+                    cmd.Parameters.AddWithValue("p_id", oDescount.IdDescount);
+
+                    cmd.Parameters.AddWithValue("p_code", oDescount.Code);
+                    cmd.Parameters.AddWithValue("p_cant", oDescount.Cant);
+                   
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+        //DELETE
+        public bool DeleteDescount(int IdDescount)
+        {
+            bool rpta;
+            try
+            {
+                var cn = new Conection();
+
+                using (var conection = new MySqlConnection(cn.getChainMySql()))
+                {
+                    conection.Open();
+                    MySqlCommand cmd = new MySqlCommand("SpDeleteDescount", conection);
+                    cmd.Parameters.AddWithValue("p_id", IdDescount);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+    }
+}
---
+++ Models/DescountModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemSales.Models
+{
+    public class DescountModel
+    {
+        public int IdDescount { get; set; }
+        [Required(ErrorMessage = "El codigo es obligatorio")]
+       
+        public string? Code { get; set; }
+        [Required]
+       
+        public string? Cant { get; set; }
+       
+    }
+}
