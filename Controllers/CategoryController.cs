2f4a324c61eba67b0c1fdd92dc6f685c103b779c
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-02-24 19:07:44 -0600

    crud categorys

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemSales.Data;
+using SistemSales.Models;
+
+namespace SistemSales.Controllers
+{
+    public class CategoryController: Controller
+    {
+        CategoryData _categoryData = new CategoryData();
+
+        public IActionResult ShowCategorys()
+        {
+            //vist of companys
+            var oList = _categoryData.ShowCategorys();
+
+            return View(oList);
+        }
+        public IActionResult CreateCategory()
+        {
+            //only vist of html formulary
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(CategoryModel oCategory)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            //save object in the database
+            var resp = _categoryData.CreateCategory(oCategory);
+
+            if (resp)
+                return RedirectToAction("ShowCategorys");
+            else
+                return View();
+        }
+        public IActionResult EditCategory(int IdCategory)
+        {
+            var oCategory = _categoryData.GetCategory(IdCategory);
+            //only vist of html formulary
+            return View(oCategory);
+        }
+        [HttpPost]
+        public IActionResult EditCategory(CategoryModel oCategory)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            //save object in the database
+            var resp = _categoryData.EditCategory(oCategory);
+
+            if (resp)
+                return RedirectToAction("ShowCategorys");
+            else
+                return View();
+        }
+        [HttpPost]
+        public IActionResult DeleteCategory(int IdCategory)
+        {
+            var resp = _categoryData.DeleteCategory(IdCategory);
+
+            if (resp)
+                return Ok(); // Si la eliminación es exitosa, devuelve una respuesta 200 OK
+            else
+                return BadRequest(); // Si hay algún error, devuelve una respuesta 400 Bad Request
+        }
+
+    }
+}
---
+++ Data/CategoryData.cs
@@ -0,0 +1,145 @@
+﻿using SistemSales.Models;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Xml.Linq;
+
+namespace SistemSales.Data
+{
+    public class CategoryData
+    {
+        public List<CategoryModel> ShowCategorys()
+        {
+            var oList = new List<CategoryModel>();
+
+            var cn = new Conection();
+
+            using (var conection = new MySqlConnection(cn.getChainMySql()))
+            {
+                conection.Open();
+                MySqlCommand cmd = new MySqlCommand("SpShowCategorys", conection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oList.Add(new CategoryModel()
+                        {
+                            IdCategory = Convert.ToInt32(dr["id"]),
+                            Name = dr["name"].ToString(),
+                            
+                        });
+                    }
+                }
+            }
+            return oList;
+        }
+        //Obtener
+        public CategoryModel GetCategory(int IdCategory)
+        {
+            var oCategory = new CategoryModel();
+
+            var cn = new Conection();
+
+            using (var conection = new MySqlConnection(cn.getChainMySql()))
+            {
+                conection.Open();
+                MySqlCommand cmd = new MySqlCommand("SpGetCategory", conection);
+                cmd.Parameters.AddWithValue("p_id", IdCategory);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+
+                        oCategory.IdCategory = Convert.ToInt32(dr["id"]);
+                        oCategory.Name = dr["name"].ToString();
+                       
+
+                    }
+                }
+            }
+            return oCategory;
+        }
+        //CREATE
+        public bool CreateCategory(CategoryModel oCategory)
+        {
+            bool rpta;
+            try
+            {
+                var cn = new Conection();
+
+                using (var conection = new MySqlConnection(cn.getChainMySql()))
+                {
+                    conection.Open();
+                    MySqlCommand cmd = new MySqlCommand("SpCreateCategory", conection);
+                    cmd.Parameters.AddWithValue("p_name", oCategory.Name);
+             
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+        //EDIT
+        public bool EditCategory(CategoryModel oCategory)
+        {
+            bool rpta;
+            try
+            {
+                var cn = new Conection();
+
+                using (var conection = new MySqlConnection(cn.getChainMySql()))
+                {
+                    conection.Open();
+                    MySqlCommand cmd = new MySqlCommand("SpEditCategory", conection);
+                    cmd.Parameters.AddWithValue("p_id", oCategory.IdCategory);
+
+                    cmd.Parameters.AddWithValue("p_name", oCategory.Name);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+        //DELETE
+        public bool DeleteCategory(int IdCategory)
+        {
+            bool rpta;
+            try
+            {
+                var cn = new Conection();
+
+                using (var conection = new MySqlConnection(cn.getChainMySql()))
+                {
+                    conection.Open();
+                    MySqlCommand cmd = new MySqlCommand("SpDeleteCategory", conection);
+                    cmd.Parameters.AddWithValue("p_id", IdCategory);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+    }
+}
